1b06547db98a86b5fbdf05d091ee1f9295a323df
matthewb2 <bestsciencekwabe2gmail.com>
2019-03-20 20:51:18 +0900

    first

---
+++ FZip/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FZip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+              OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "파일 오픈 예제창";
+            ofd.FileName = "test";
+            ofd.Filter = "그림 파일 (*.jpg, *.gif, *.bmp) | *.jpg; *.gif; *.bmp; | 모든 파일 (*.*) | *.*";
+ 
+            //파일 오픈창 로드
+            DialogResult dr = ofd.ShowDialog();
+             
+            //OK버튼 클릭시
+            if (dr == DialogResult.OK)
+            {
+                //File명과 확장자를 가지고 온다.
+                string fileName = ofd.SafeFileName;
+                //File경로와 File명을 모두 가지고 온다.
+                string fileFullName = ofd.FileName;
+                //File경로만 가지고 온다.
+                string filePath = fileFullName.Replace(fileName, "");
+
+
+                insertList();
+
+            }
+
+        }
+        public void insertList(){
+            // Create a new ListView control.
+           
+            // Set the view to show details.
+            listView1.View = View.Details;
+            // Allow the user to edit item text.
+            listView1.LabelEdit = true;
+            // Allow the user to rearrange columns.
+            listView1.AllowColumnReorder = true;
+            // Display check boxes.
+            listView1.CheckBoxes = true;
+            // Select the item and subitems when selection is made.
+            listView1.FullRowSelect = true;
+            // Display grid lines.
+            listView1.GridLines = true;
+            // Sort the items in the list in ascending order.
+            listView1.Sorting = SortOrder.Ascending;
+
+            // Create three items and three sets of subitems for each item.
+            ListViewItem item1 = new ListViewItem("item1", 0);
+            // Place a check mark next to the item.
+            item1.Checked = true;
+            item1.SubItems.Add("1");
+            item1.SubItems.Add("2");
+            item1.SubItems.Add("3");
+            ListViewItem item2 = new ListViewItem("item2", 1);
+            item2.SubItems.Add("4");
+            item2.SubItems.Add("5");
+            item2.SubItems.Add("6");
+            ListViewItem item3 = new ListViewItem("item3", 0);
+            // Place a check mark next to the item.
+            item3.Checked = true;
+            item3.SubItems.Add("7");
+            item3.SubItems.Add("8");
+            item3.SubItems.Add("9");
+
+            // Create columns for the items and subitems.
+            // Width of -2 indicates auto-size.
+           
+            //Add the items to the ListView.
+            listView1.Items.AddRange(new ListViewItem[] { item1, item2, item3 });
+
+            // Create two ImageList objects.
+            ImageList imageListSmall = new ImageList();
+            ImageList imageListLarge = new ImageList();
+
+            // Initialize the ImageList objects with bitmaps.
+         
+            //Assign the ImageList objects to the ListView.
+            listView1.LargeImageList = imageListLarge;
+            listView1.SmallImageList = imageListSmall;
+
+            // Add the ListView to the control collection.
+            this.Controls.Add(listView1);
+           }
+
+    }
+}
